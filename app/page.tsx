78d9bede501d2a501c0bca688fb632079f4e9f65
mdmims <mdmims.tech@gmail.com>
2023-06-19 15:38:25 -0400

    init homepage styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">My Journal App</h1>
+        <p className="text-3xl text-white/60 mb-4">Built to track your mood over time. Just be honest!</p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+              get started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
