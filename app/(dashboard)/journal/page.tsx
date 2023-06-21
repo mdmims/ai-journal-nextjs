a02b35126f0b5d54fd3bd781ead9d5b286315b4d
mdmims <mdmims.tech@gmail.com>
2023-06-21 09:06:58 -0400

    add prisma queries and entry components, auth helper func to map Clerk persona to PlanetScale user

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { prisma } from '@/utils/db';
+import { getUserByClerkId } from '@/utils/auth';
+import NewEntryCard from '@/components/NewEntryCard';
+import EntryCard from '@/components/EntryCard';
 
-const JournalPage = () => {
+const getEntries = async () => {
+  const user = await getUserByClerkId();
+  return prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+};
+
+const JournalPage = async () => {
+  const entries = await getEntries();
+
   return (
-      <div>
-        journal
+    <div className="p-10 bg-zinc-400/10 h-full">
+      <h2 className="text-3xl mb-8">Journal</h2>
+      <div className="grid grid-cols-3 gap-4">
+      <NewEntryCard />
+      {entries.map((entry) => (
+        <EntryCard key={entry.id} entry={entry} />
+      ))}
+    </div>
       </div>
   );
 };
---
+++ components/EntryCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EntryCard = ({ entry }) => {
+  return (
+      <div>
+        {entry.id}
+      </div>
+  );
+};
+
+export default EntryCard;
